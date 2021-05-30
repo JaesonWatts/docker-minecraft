3514cad8eceab5f9f14c8b9e7b4ca878a579e1a8
Matthew X. Economou <xenophon@irtnog.org>
2021-05-30 10:17:44 -0400

    feat(*): define the initial set of container images

---
+++ README.md
@@ -0,0 +1,27 @@
+# docker-minecraft
+
+This repository defines several Docker images used to host Minecraft servers.
+Deployers may bring their own configurations, worlds, and modpacks through the use of volumes (preferred) or bind mounts.
+
+# Installation
+
+Run the `build.sh` script included with this repository create Docker images for:
+
+- the latest official Minecraft server
+
+- the latest recommended Minecraft Forge server (includes the Chunk Pregenerator mod)
+
+# Deployment
+
+Start a container using a command similar to the following:
+
+```sh
+docker run -d -p 25565:25565 --name minecraft --restart unless-stopped \
+    -v minecraft:/server/data minecraft-forge-server:1.16.5-36.1.0
+```
+
+# Contributing
+
+This project uses [GitHub Flow](https://githubflow.github.io/) except that the always-deployable branch is `main`.
+
+This project follows the [angular.js Git Commit Guidelines](https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#-git-commit-guidelines).
---
+++ minecraft-server/start.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -eux
+
+# Check EULA
+if [ ! -e eula.txt ]; then
+    if [ "$EULA" != "" ]; then
+        echo "# Generated via Docker on $(date)" > eula.txt
+        echo "eula=$EULA" >> eula.txt
+    else
+        echo 'ERROR: The Minecraft End User License Agreement (EULA) must be accepted before the server can be started.\n'
+        echo "By setting the enviroment variable EULA to TRUE you are indicating your agreement to the EULA at https://account.mojang.com/documents/minecraft_eula."
+        exit 1
+    fi
+fi
+
+java -Xmx${JVM_XMX:-4G} -Xms${JVM_XMS:-4G} \
+     -XX:+ParallelRefProcEnabled \
+     -XX:MaxGCPauseMillis=200 \
+     -XX:+UnlockExperimentalVMOptions \
+     -XX:+DisableExplicitGC \
+     -XX:+AlwaysPreTouch \
+     -XX:G1NewSizePercent=30 \
+     -XX:G1MaxNewSizePercent=40 \
+     -XX:G1HeapRegionSize=8M \
+     -XX:G1ReservePercent=20 \
+     -XX:G1HeapWastePercent=5 \
+     -XX:G1MixedGCCountTarget=4 \
+     -XX:InitiatingHeapOccupancyPercent=15 \
+     -XX:G1MixedGCLiveThresholdPercent=90 \
+     -XX:G1RSetUpdatingPauseTimePercent=5 \
+     -XX:SurvivorRatio=32 \
+     -XX:+PerfDisableSharedMem \
+     -XX:MaxTenuringThreshold=1 \
+     -Dusing.aikars.flags=https://mcflags.emc.gs \
+     -Daikars.new.flags=true \
+     -jar /server/server.jar nogui
