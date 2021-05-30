800fc44ad591b3c84d9c4e780342b34c1b24ebb9
Matthew X. Economou <xenophon@irtnog.org>
2021-05-30 11:23:20 -0400

    feat(minecraft-forge-server): move the Chunk Pregenerator download into a build stage

--- build.sh
+++ build.sh
@@ -2,21 +2,12 @@
 
 set -Eeuxo pipefail
 
-docker rmi minecraft-forge-server:1.16.5-36.1.0 || true
-docker rmi chunk-pregenerator:1.16-3.0.5 || true
-docker rmi curseforge-downloader:latest || true
-docker rmi minecraft-server:1.16.5 || true
-
 pushd minecraft-server
 docker build --force-rm -t minecraft-server:1.16.5 .
 popd
 
 pushd curseforge-downloader
 docker build --force-rm -t curseforge-downloader:latest .
-popd
-
-pushd chunk-pregenerator
-docker build --force-rm -t chunk-pregenerator:1.16-3.0.5 .
 popd
 
 pushd minecraft-forge-server
